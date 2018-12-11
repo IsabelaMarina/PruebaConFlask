6dc888790a210acb9550fdb2e819bd5bcca554a9
IsabelaMarina <45767892+IsabelaMarina@users.noreply.github.com>
2018-12-11 09:19:59 -0500

    Update pruebas.py

--- pruebas.py
+++ pruebas.py
@@ -10,57 +10,42 @@ def acerca_de():
 
 @app.route('/acerca/json')
 def acerca_de_json():
-    saludo = """Programa de Phyton creado en terminal<br>
-    Creado por: Isabela Mercado<br>
-    Fecha: 10-dic-2018"""
+    saludo = acerca_de()
     return jsonify(resultado = saludo)
 
-@app.route('/emoticon')
-def emoticon():
-    emoticono = "Prueba de emoticono 😉"
-    return emoticono
-
-@app.route('/emoticon/json')
-def emoticon_json():
-    emoticono = "Prueba de emoticono 😉"
-    return jsonify(mostrar = emoticono)
-
 @app.route('/suma/<a>/<b>')
-def sumas(a,b):
-    a = int(a)
-    b = int(b)
-    res = a + b
+def sumas(a, b):
+    try:
+        if a == '' or b == '':
+            res = "-> Error"
+        else:
+            a = float(a)
+            b = float(b)
+            res = a + b
+    except:
+        res = "-> Error"
     muestra = "El resultado es " + str(res)
     return muestra
 
 @app.route('/suma/<a>/<b>/json')
 def sumas_json(a,b):
-    a = int(a)
-    b = int(b)
-    res = a + b
-    muestra = "El resultado es " + str(res)
+    muestra = sumas(a,b)
     return jsonify(resultado = muestra)
 
 @app.route('/divide/<a>/<b>')
 def divides(a,b):
-    a = int(a)
-    b = int(b)
     try:
+        a = float(a)
+        b = float(b)
         res = a / b
     except:
-        res = "-> Error de división por cero"
+        res = "-> Error"
     muestra = "El resultado es " + str(res)
     return muestra
 
 @app.route('/divide/<a>/<b>/json')
 def divides_json(a,b):
-    a = int(a)
-    b = int(b)
-    try:
-        res = a / b
-    except:
-        res = "-> Error de división por cero"
-    muestra = "El resultado es " + str(res)
+    muestra = divides(a,b)
     return jsonify(resultado = muestra)
 
 if __name__ == '__main__':
